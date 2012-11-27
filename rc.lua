9d27f5f52b994b9f2ffaefe7345dbe668b1cd076
Matthias Brunisholz <hidekin1982@gmail.com>
2012-11-27 21:49:44 +0100

    added libreoffice rules tag

--- rc.lua
+++ rc.lua
@@ -80,7 +80,8 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ "1⇋ Main", "2⇋ Admin.", "3⇋ www/Web", "4⇋ Terminal", "5⇋ Video","6⇋ Audio","7⇋ Development"}, s, layouts[1])
+    tags[s] = awful.tag({ "1⇋ Main", "2⇋ Admin.", "3⇋ www/Web", "4⇋ Terminal", "5⇋ Video","6⇋ Audio","7⇋ Development","8⇋ Office"}, s, 
+layouts[1])
 end
 -- }}}
 
@@ -403,6 +404,19 @@ awful.rules.rules = {
        properties = { tag = tags[1][7] } },
 { rule = { class = "Pcmanfm" },
        properties = { tag = tags[1][1] } },
+{ rule = { class = "libreoffice --writer" },
+       properties = { tag = tags[1][8] } },
+{ rule = { class = "libreoffice --base" },
+       properties = { tag = tags[1][8] } },
+{ rule = { class = "libreoffice --impress" },
+       properties = { tag = tags[1][8] } },
+{ rule = { class = "libreoffice --writer" },
+       properties = { tag = tags[1][8] } },
+{ rule = { class = "libreoffice --calc" },
+       properties = { tag = tags[1][8] } },
+
+
+
 }
 -- }}}
 
